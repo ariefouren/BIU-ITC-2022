f8a853942e054b98dabc30d514fc0cc4e0442201
ariefouren <aporan@ono.ac.il>
2023-01-04 21:00:48 +0200

    XX

--- ‏‏‏‏‏‏‏‏‏‏‏‏Lesson_10_list_sorting/sortedList.cpp
+++ ‏‏‏‏‏‏‏‏‏‏‏‏Lesson_10_list_sorting/sortedList.cpp
@@ -37,8 +37,48 @@ void insertToSortedList(Node* head, Node* newNode)
     {
         prevNode = prevNode->next;
     }
-    
+    newNode->next = prevNode->next;
+    prevNode->next = newNode;
 }
+
+void printList(Node* head)
+{
+    Node* currNode = head->next;
+    while(currNode->next != NULL)
+    {
+        cout << setw(WIDTH) << currNode->value;
+        currNode = currNode->next;
+    }
+}
+
+int main()
+{
+    Node* sortedList = createEmptyList();
+
+    int numValues;
+    cout << "Enter the number of values to sort : ";
+    cin >> numValues;
+    
+    // generate random values and insert them into the unsorted list
+    srand (time(NULL));
+    for(int i = 1; i<=numValues; i++)
+    {
+        // create a new node with a random value and insert it into the sorted list
+        int newValue = rand();
+        Node* newNode = createNewNode(newValue);
+        insertToSortedList(sortedList, newNode);
+    }
+
+    // print the sorted list of the values
+    cout << "Sorted list of the values: " << endl;
+    printList(sortedList);
+    cout<<endl;
+
+    system("pause");
+    return 0;
+}    
+
+
 
 
 

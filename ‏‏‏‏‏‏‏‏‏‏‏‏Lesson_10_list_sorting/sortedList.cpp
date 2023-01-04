d9ac7631f1b828b379d3d3a1c0315749cee7c4ba
ariefouren <aporan@ono.ac.il>
2023-01-04 13:41:28 +0200

    XX

--- ‏‏‏‏‏‏‏‏‏‏‏‏Lesson_10_list_sorting/sortedList.cpp
+++ ‏‏‏‏‏‏‏‏‏‏‏‏Lesson_10_list_sorting/sortedList.cpp
@@ -7,3 +7,38 @@
 #define WIDTH 10
 using namespace std; 
 
+struct Node
+{
+    int value;
+    Node* next;
+};
+
+Node* createNewNode(int val)
+{
+    Node* newNode = new Node;
+    newNode->next = NULL;
+    newNode->value = val;
+    return newNode;
+}
+
+Node* createEmptyList()
+{
+    Node* firstSentinel = createNewNode(INT_MIN);
+
+    Node* lastSentinel = createNewNode(INT_MAX);
+    firstSentinel->next = lastSentinel;
+    return firstSentinel;
+}
+
+void insertToSortedList(Node* head, Node* newNode)
+{
+    Node* prevNode = head; 
+    while(prevNode->next->value < newNode->value)
+    {
+        prevNode = prevNode->next;
+    }
+    
+}
+
+
+
